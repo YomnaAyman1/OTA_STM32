069bd3ba67f02d9b1b2953d066bcb0e47dca2066
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-26 23:09:15 +0200

    Update GPIO_config.h

--- 04 - BL_APP/include/GPIO_config.h
+++ 04 - BL_APP/include/GPIO_config.h
@@ -1,7 +1,7 @@
 /*****************************************************/
- /* Author      : Abdelrhman Mosad                   */
+ /* Author      : Yomna Ayman                        */
  /* Version     : v01 (Bl)                           */
- /* date        : 21/10/2020                         */
+ /* date        : 11/10/202                          */
 /*****************************************************/
 #ifndef GPIO_CONFIG_H
 #define GPIO_CONFIG_H
