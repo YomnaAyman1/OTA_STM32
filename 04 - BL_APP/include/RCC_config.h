1cb44aa226c3a5e62702b19f9f655c7a2ce6fcde
YomnaAyman1 <68161203+YomnaAyman1@users.noreply.github.com>
2023-11-26 23:15:00 +0200

    Update RCC_config.h

--- 04 - BL_APP/include/RCC_config.h
+++ 04 - BL_APP/include/RCC_config.h
@@ -1,7 +1,7 @@
 /************************************************/
-/* Author      : Yomna Ayman                         */
-/* Version     : v01 (Bl)                            */
-/* date        : 11/10/2022                          */
+/* Author      : Yomna Ayman                    */
+/* Version     : v01 (Bl)                       */
+/* date        : 11/10/2022                     */
 /************************************************/
 #ifndef _RCC_CONFIG_H
 #define _RCC_CONFIG_H
